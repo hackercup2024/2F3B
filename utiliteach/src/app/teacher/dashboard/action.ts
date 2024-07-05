7d659ef36053fde75ba30c8dd7500e6084e3c66e
YUNa <jahitosis1@up.edu.ph>
2024-07-06 01:17:43 +0800

    ✨: ewan

---
+++ utiliteach/src/app/teacher/dashboard/action.ts
@@ -0,0 +1,47 @@
+"use server";
+
+import { db } from "@/db";
+
+export const getAttendanceTally = async ({
+  sectionId
+}: {
+    sectionId: number
+  }) => {
+  const result = await db.student.findMany({
+    where: {
+      sectionId: sectionId,
+    },
+    select: {
+      id: true,
+      firstName: true,
+      lastName: true,
+      Attendance: {
+        select: {
+          id: true,
+        },
+      },
+    },
+  });
+
+  // Transform the result to get the total attendance count per student
+  const attendancePerStudent = result.map(student => ({
+    studentId: student.id,
+    firstName: student.firstName,
+    lastName: student.lastName,
+    totalAttendance: student.Attendance.length,
+  }));
+
+  return attendancePerStudent;
+};
+
+export const getSubjects = async () => {
+  return await db.subject.findMany();
+}
+
+export const getSectionBySubject = async ({
+  subjectId
+}: {
+    subjectId: number
+  }) => {
+  return await db.section.findMany({ where: { subjectId } });
+}
--- utiliteach/src/app/teacher/dashboard/page.tsx
+++ utiliteach/src/app/teacher/dashboard/page.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 "use client"
 
 import {
@@ -6,16 +7,26 @@ import {
   SelectItem,
   SelectTrigger,
   SelectValue,
-  SelectGroup,
-  SelectLabel,
 } from "@/components/ui/select"
 import { Button } from "@/components/ui/button";
-import { useState } from "react"
+import { useEffect, useState } from "react"
+import { getAttendanceTally } from "./action";
 
 const Dashboard = () => {
 
   const [selectedClass, setSelectedClass] = useState<string>('');
-  const [selectedSection, setSelectedSection] = useState<string>('');
+  const [selectedSection, setSelectedSection] = useState<number>(-1);
+  let attendanceTally: { studentId: number; firstName: string; lastName: string; totalAttendance: number; }[] = []
+
+  useEffect(() => {
+    if (selectedSection === -1) return;
+    fetchAttendanceTally();
+    console.log(attendanceTally);
+  }, [selectedSection]);
+
+  const fetchAttendanceTally = async () => {
+    attendanceTally = await getAttendanceTally({ sectionId: selectedSection })
+  }
 
   return (
     <div className="flex flex-grow flex-col items-center p-4 gap-3">
@@ -31,18 +42,18 @@ const Dashboard = () => {
             <SelectItem value="clt">Chile Standard Time (CLT)</SelectItem>
           </SelectContent>
         </Select>
-        <Select onValueChange={e => setSelectedSection(e)} disabled={selectedClass === ''}>
+        <Select onValueChange={e => setSelectedSection(parseInt(e))} disabled={selectedClass === ''}>
           <SelectTrigger className="w-[280px]">
             <SelectValue placeholder="Select a Section" />
           </SelectTrigger>
           <SelectContent>
-            <SelectItem value="art">Argentina Time (ART)</SelectItem>
-            <SelectItem value="bot">Bolivia Time (BOT)</SelectItem>
-            <SelectItem value="brt">Brasilia Time (BRT)</SelectItem>
-            <SelectItem value="clt">Chile Standard Time (CLT)</SelectItem>
+            <SelectItem value="0">Argentina Time (ART)</SelectItem>
+            <SelectItem value="1">Bolivia Time (BOT)</SelectItem>
+            <SelectItem value="2">Brasilia Time (BRT)</SelectItem>
+            <SelectItem value="3">Chile Standard Time (CLT)</SelectItem>
           </SelectContent>
         </Select>
-        <Button className="w-full text-black bg-white border border-black" disabled={selectedSection === ''}>Start Class</Button>
+        <Button className="w-full text-black bg-white border border-black" disabled={selectedSection === -1}>Start Class</Button>
       </div>
       <div className="w-fit flex max-md:flex-col max-md:items-center md:justify-center gap-3">
       </div>
