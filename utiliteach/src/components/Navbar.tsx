6c865fe9cdb54c989673abccad22dfd8843fc129
YUNa <jahitosis1@up.edu.ph>
2024-07-06 00:24:50 +0800

    ✨: initial teacher dashboard

--- utiliteach/src/components/Navbar.tsx
+++ utiliteach/src/components/Navbar.tsx
@@ -11,7 +11,7 @@ const Navbar = async () => {
   return (
     <nav
       className="sticky inset-x-0 top-0 z-[100] h-14 w-full border-b border-gray-200 bg-white/75
-        backdrop-blur-lg transition-all"
+        backdrop-blur-lg transition-all overflow-y-hidden"
     >
       <MaxWidthWrapper>
         <div className="flex h-14 items-center justify-between border-b border-zinc-200">
