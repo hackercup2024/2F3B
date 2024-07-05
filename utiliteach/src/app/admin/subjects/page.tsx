457b6eb6f147546c147f03c971a329a0b0035a9f
cyalbino <cpalbino@up.edu.ph>
2024-07-06 01:59:54 +0800

    🔨:subject ui

--- utiliteach/src/app/admin/subjects/page.tsx
+++ utiliteach/src/app/admin/subjects/page.tsx
@@ -1,10 +1,67 @@
-const Subjects = () => {
+"use client";
+import { useQuery } from "@tanstack/react-query";
+import {
+  Table,
+  TableBody,
+  TableCaption,
+  TableCell,
+  TableFooter,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
+import CreateSubject from "./CreateSubject";
+import { getSubjects } from "./action";
+import { Subject } from "@prisma/client";
+
+const Page = () => {
+  const {
+    data: subjects,
+    isLoading,
+    isError,
+  } = useQuery({
+    queryKey: ["get-subjects-key"],
+    queryFn: async () => await getSubjects(),
+    retry: true,
+    retryDelay: 500,
+  });
+
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+
+  if (isError) {
+    return <div>Error loading subjects.</div>;
+  }
+
   return (
-    <>
-      Subjects
-    </>
+    <div className="p-4">
+      <div className="flex justify-center sm:justify-start">
+        <h1 className="text-lapis font-bold text-2xl mb-4">Subject Management</h1>
+      </div>
+      <CreateSubject />
+      <div className="flex flex-col items-center">
+        <Table className="sm:w-1/2">
+          <TableCaption>A list of your subjects.</TableCaption>
+          <TableHeader>
+            <TableRow>
+              <TableHead className="text-center">Subject Name</TableHead>
+              <TableHead className="text-center">Grade Level</TableHead>
+            </TableRow>
+          </TableHeader>
+          <TableBody>
+            {subjects?.map((subject: Subject) => (
+              <TableRow key={subject.id}>
+                <TableCell className="text-center">{subject.name}</TableCell>
+                <TableCell className="text-center">{subject.gradeLevel}</TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </div>
+      <div className="max-sm:mb-24"></div>
+    </div>
   );
-}
+};
 
-export default Subjects;
-
+export default Page;
