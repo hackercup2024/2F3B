3f778d5e2cb6b39caa9c5025fdcc3446c493c670
cyalbino <cpalbino@up.edu.ph>
2024-07-06 00:29:54 +0800

    ✨: added qna in landing page

--- utiliteach/tailwind.config.ts
+++ utiliteach/tailwind.config.ts
@@ -19,6 +19,8 @@ const config = {
     },
     extend: {
       colors: {
+        eraser: "#9A0B05",
+        lapis: "#FFD230",
         border: "hsl(var(--border))",
         input: "hsl(var(--input))",
         ring: "hsl(var(--ring))",
