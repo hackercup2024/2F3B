d833462b5a3b89a112c9dcc6c12ff0ed5743cc02
YUNa <jahitosis1@up.edu.ph>
2024-07-05 23:13:04 +0800

    🔨: added max-md:hidden to footer

--- utiliteach/src/components/Footer.tsx
+++ utiliteach/src/components/Footer.tsx
@@ -3,7 +3,7 @@ import MaxWidthWrapper from "./MaxWidthWrapper";
 
 const Footer = () => {
   return (
-    <footer className="bg-white h-20 relative">
+    <footer className="bg-white h-20 relative max-md:hidden">
       <MaxWidthWrapper>
         <div className="border-t border-gray-200" />
         <div className="h-full flex flex-col md:flex-row md:justify-between justify-center items-center">
