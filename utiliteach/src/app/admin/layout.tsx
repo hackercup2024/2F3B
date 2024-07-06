6ccd0869a2321bfe70771329bf72b4763fd0384e
cocoman043 <rocampita@up.edu.ph>
2024-07-06 08:30:00 +0800

    🔨 fixed route auth

--- utiliteach/src/app/admin/layout.tsx
+++ utiliteach/src/app/admin/layout.tsx
@@ -1,35 +1,45 @@
-"use client"
 import "../globals.css";
 import { constructMetaData } from "@/lib/utils";
 import Tabs from "@/components/ui/tabs";
-import { LucideBook, LucideBookUser, LucideFile, LucideUsersRound } from "lucide-react";
+import {
+  LucideBook,
+  LucideBookUser,
+  LucideFile,
+  LucideUsersRound,
+} from "lucide-react";
 import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
-import { useEffect } from "react";
-import { notFound } from "next/navigation";
+// import { useEffect } from "react";
+import { notFound, useRouter } from "next/navigation";
 
 // export const metadata = constructMetaData();
 
-export default function RootLayout({
+export default async function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  // const router = useRouter();
 
-  useEffect(() => {
-    async function protect() {
-      const {getUser} = getKindeServerSession();
-      const user = await getUser();
+  // useEffect(() => {
+  //   async function protect() {
+  //     const { getUser } =  getKindeServerSession();
+  //     const user = await getUser();
+  //
+  //     if (!user) return router.push("/");
+  //   }
+  //   protect();
+  // }, []);
 
-      if (!user) return notFound();
-    }
-    protect();
-  }, [])
+  const { getUser } = getKindeServerSession();
+  const user = await getUser();
+
+  if (!user) return notFound();
   const links = [
-    {link: '/admin/teachers', icon: <LucideBookUser />, label: "Teachers"},
-    {link: '/admin/subjects', icon: <LucideBook />, label: "Subjects"},
-    {link: '/admin/classes', icon: <LucideUsersRound />, label: "Sections"},
-    {link: '/admin/reports', icon: <LucideFile />, label: "Reports"},
-  ]
+    { link: "/admin/teachers", icon: <LucideBookUser />, label: "Teachers" },
+    { link: "/admin/subjects", icon: <LucideBook />, label: "Subjects" },
+    { link: "/admin/classes", icon: <LucideUsersRound />, label: "Sections" },
+    { link: "/admin/reports", icon: <LucideFile />, label: "Reports" },
+  ];
 
   return (
     <>
@@ -38,4 +48,3 @@ export default function RootLayout({
     </>
   );
 }
-
